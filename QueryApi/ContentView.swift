4c11e74454940dab31fb47e3d3398f700e1dd50f
erwinagpasa <rootaccess000@gmail.com>
2023-10-14 11:41:50 +0300

    fixing

---
+++ QueryApi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QueryApi
+//
+//  Created by Developer on 14/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QueryApi/QueryApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  QueryApiApp.swift
+//  QueryApi
+//
+//  Created by Developer on 14/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct QueryApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
